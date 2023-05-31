73b31d542f2fc2ab1999a5f803f9d11b9fe867ee
maye156 <maye156@gmail.com>
2023-05-30 20:45:52 -0500

    Primera app funcional

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-
+import Index from "./pages/index";
+import Create from "./pages/create";
+import View from "./pages/view";
+import { Routes, Route, BrowserRouter  } from "react-router-dom";
+import Store from "./store/store";
 
 function App() {
   return (
-    <div >
-      Proyecto inicial 
-    </div>
+    <Store>
+      <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="create" element={<Create />} />
+          <Route path="view/:bookId" element={<View />} />
+        </Routes>
+        </BrowserRouter>
+      </div>
+    </Store>
   );
 }
 
---
+++ src/components/layout.js
@@ -0,0 +1,14 @@
+import NavBar from "./navar";
+
+export default function Layout({ children }) {
+  const containerStyle = {
+    width: "90%",
+    margin: "100px auto",
+  };
+  return (
+    <div>
+      <NavBar />
+      <div style={containerStyle}>{children}</div>
+    </div>
+  );
+}
